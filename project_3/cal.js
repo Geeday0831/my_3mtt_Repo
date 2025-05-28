3cc1c743634d9000df5bf02e36fa10ad49ed6850
abdulrauf <abdulraufabdulwahab3108@gmail.com>
2025-05-28 14:19:47 +0100

    project 3

---
+++ project_3/cal.js
@@ -0,0 +1,68 @@
+/* variable declarations  */
+
+
+let display = document.getElementById('display');
+let currentInput = '';
+let operator = '';
+let previousInput = '';
+
+/* Event Listeners */
+
+function appendNumber(number) {
+    currentInput += number;
+    display.value = currentInput;
+}
+
+/* appneding function */
+
+function appendOperator(op) {
+    if (currentInput === '' && op !== '-') return; 
+    // prevent adding operator at the start except for '-'
+    if (operator !== '') calculate(); 
+    
+    //to help me Calculate if there's already an operator
+    operator = op;
+    previousInput = currentInput;
+    currentInput = '';
+    display.value = previousInput + ' ' + operator + ' ';
+}
+
+
+/* To calculate the final result*/
+function calculate() {
+    if (previousInput === '' || currentInput === '') return; //this helps me  Prevent calculation if inputs are incomplete
+    let result;
+    const prev = parseFloat(previousInput);
+    const curr = parseFloat(currentInput);
+    switch (operator) {
+        case '+':
+            result = prev + curr;
+            break;
+        case '-':
+            result = prev - curr;
+            break;
+        case '*':
+            result = prev * curr;
+            break;
+        case '/':
+            result = prev / curr;
+            break;
+        default:
+            return;
+    }
+    display.value = result;
+    currentInput = result.toString();
+    operator = '';
+    previousInput = '';
+}
+
+
+/* clear screen display */
+
+
+function clearDisplay() {
+    currentInput = '';
+    operator = '';
+    previousInput = '';
+    display.value = '';
+}
